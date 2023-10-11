d9b756da448e8068b721f7dc2aabbeeb0f2c3d26
KevSalad <112964203+KevSalad@users.noreply.github.com>
2023-10-11 14:25:23 -0500

    16x16 Responsive Grid

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// Create the divs using JavaScript. Let's start with a simple grid of 16x16 squares.
+// Function to create a 16x16 grid
 function createGrid() {
-    
-    var body = document.body;
-    
-    // Create a container div for the grid
-    var gridContainer = document.createElement('div');
-    gridContainer.className = 'grid-container';
+    var gridContainer = document.getElementById('gridContainer');
 
-    // Create the grid divs
     for (var i = 0; i < 16; i++) {
-        // Create a row div
         var row = document.createElement('div');
         row.className = 'grid-row';
 
-        // Loop to create 16 columns in each row
         for (var j = 0; j < 16; j++) {
-            // Create a grid item (cell)
             var gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
 
-            // Add the grid item to the row
+            // Add event listeners for hover effect
+            gridItem.addEventListener('mouseenter', function() {
+                this.style.backgroundColor = 'lightblue'; // Change background color on hover
+            });
+
+            gridItem.addEventListener('mouseleave', function() {
+                this.style.backgroundColor = 'lightgray'; // Change back to default color on hover out
+            });
+
             row.appendChild(gridItem);
         }
-        // Append the row to the grid container
+
         gridContainer.appendChild(row);
     }
-    // Append the grid container to the body
-    body.appendChild(gridContainer);
 }
 
 // Call the function to create the grid
 createGrid();
+
+
+
 
